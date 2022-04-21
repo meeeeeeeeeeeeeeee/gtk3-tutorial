2b26f9b25987b6c412d6f637ec7f58d4a45d6fa9
meeeeeeeeeeeeeeee <42124824+meeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-21 11:56:17 -0500

    Update aboutdialog.c

--- _examples/aboutdialog.c
+++ _examples/aboutdialog.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
     gtk_init(&argc, &argv);
     
     GtkWidget *aboutdialog = gtk_about_dialog_new();
-    gtk_about_dialog_set_program_name (GTK_ABOUT_DIALOG(about_dialog), "Learn GTK+");
+    gtk_about_dialog_set_program_name (GTK_ABOUT_DIALOG(aboutdialog), "Learn GTK+");
     gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(aboutdialog), "1.0");
     gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(aboutdialog), "An introduction to using GTK+");
     gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(aboutdialog), "http://learngtk.org/");
