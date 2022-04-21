df4a1c8b5a54895997a9ca86c59dc7ea88dadda3
meeeeeeeeeeeeeeee <42124824+meeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-21 12:27:28 -0500

    Update combobox.c

--- _examples/combobox.c
+++ _examples/combobox.c
@@ -1,12 +1,7 @@
 #include <gtk/gtk.h>
 
-static void destroy(GtkWidget *widget, gpointer data)
-{
-    gtk_main_quit();
-}
+int main(int argc, char *argv[]) {    
 
-int main(int argc, char *argv[])
-{    
     gtk_init(&argc, &argv);
     
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -16,15 +11,15 @@ int main(int argc, char *argv[])
     GtkTreeIter iter;
     
     gtk_list_store_append(liststore, &iter);
-    gtk_list_store_set(liststore, &iter, 0, "Fedora", -1);
+    gtk_list_store_set(liststore, &iter, 0, "Item 1", -1);
     gtk_list_store_append(liststore, &iter);
-    gtk_list_store_set(liststore, &iter, 0, "Debian", -1);
+    gtk_list_store_set(liststore, &iter, 0, "Item 2", -1);
     gtk_list_store_append(liststore, &iter);
-    gtk_list_store_set(liststore, &iter, 0, "OpenSuSE", -1);
+    gtk_list_store_set(liststore, &iter, 0, "Item 3", -1);
     gtk_list_store_append(liststore, &iter);
-    gtk_list_store_set(liststore, &iter, 0, "Sabayon", -1);
+    gtk_list_store_set(liststore, &iter, 0, "Item 4", -1);
     gtk_list_store_append(liststore, &iter);
-    gtk_list_store_set(liststore, &iter, 0, "CentOS", -1);
+    gtk_list_store_set(liststore, &iter, 0, "Item 5", -1);
     
     GtkWidget *combobox = gtk_combo_box_new_with_model(GTK_TREE_MODEL(liststore));
     gtk_combo_box_set_active(GTK_COMBO_BOX(combobox), 0);
@@ -34,9 +29,12 @@ int main(int argc, char *argv[])
     gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(combobox), cellrenderertext, TRUE);
     gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(combobox), cellrenderertext, "text", 0, NULL);
     
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    
     gtk_widget_show_all(window);
     
     gtk_main();
     
     return 0;
+
 }
