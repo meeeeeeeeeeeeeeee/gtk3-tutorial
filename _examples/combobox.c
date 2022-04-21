2cf542b95a95372b1e34dfba901ec07b9553cd09
meeeeeeeeeeeeeeee <42124824+meeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-21 12:45:07 -0500

    Update combobox.c

--- _examples/combobox.c
+++ _examples/combobox.c
@@ -3,13 +3,14 @@
 int main(int argc, char *argv[]) {    
 
     gtk_init(&argc, &argv);
-    
+
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
-    
+
     GtkListStore *liststore = gtk_list_store_new(1, G_TYPE_STRING);
     GtkTreeIter iter;
-    
+    GtkWidget *combobox = gtk_combo_box_new_with_model(GTK_TREE_MODEL(liststore));
+    GtkCellRenderer *cellrenderertext = gtk_cell_renderer_text_new();
+
     gtk_list_store_append(liststore, &iter);
     gtk_list_store_set(liststore, &iter, 0, "Item 1", -1);
     gtk_list_store_append(liststore, &iter);
@@ -20,12 +21,10 @@ int main(int argc, char *argv[]) {
     gtk_list_store_set(liststore, &iter, 0, "Item 4", -1);
     gtk_list_store_append(liststore, &iter);
     gtk_list_store_set(liststore, &iter, 0, "Item 5", -1);
-    
-    GtkWidget *combobox = gtk_combo_box_new_with_model(GTK_TREE_MODEL(liststore));
+
     gtk_combo_box_set_active(GTK_COMBO_BOX(combobox), 0);
     gtk_container_add(GTK_CONTAINER(window), combobox);
-    
-    GtkCellRenderer *cellrenderertext = gtk_cell_renderer_text_new();
+
     gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(combobox), cellrenderertext, TRUE);
     gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(combobox), cellrenderertext, "text", 0, NULL);
     
