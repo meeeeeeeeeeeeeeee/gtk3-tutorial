acec0c72f6ba807085552db229ba666ee60961da
meeeeeeeeeeeeeeee <42124824+meeeeeeeeeeeeeeee@users.noreply.github.com>
2022-04-29 22:39:07 -0500

    Update filesavedialog.c

--- _examples/filesavedialog.c
+++ _examples/filesavedialog.c
@@ -7,8 +7,8 @@ int main(int argc, char *argv[]) {
     GtkWidget       *file_save_dialog = gtk_file_chooser_dialog_new("Save File", NULL, GTK_FILE_CHOOSER_ACTION_SAVE, "Cancel", GTK_RESPONSE_CANCEL, "Save", GTK_RESPONSE_OK, NULL);
 
     gtk_dialog_run(GTK_DIALOG(file_save_dialog));
-	  g_print("Selected file: %s", gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(file_save_dialog)));
-	  gtk_widget_destroy(file_save_dialog);
+    g_print("Selected file: %s", gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(file_save_dialog)));
+    gtk_widget_destroy(file_save_dialog);
 
     return 0;
 
